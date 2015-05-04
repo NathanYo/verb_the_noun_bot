4e4cef37abfe0c303e5af2aa3ae761e5d63620c4
Jeremy R Jones <jeremyjones@ecc-c-23vm14.rd.unr.edu>
2015-05-03 22:35:10 -0700

    Updated jeremy_bot to check for duplicate tweets before tweeting

--- jeremy_bot.py
+++ jeremy_bot.py
@@ -1,16 +1,35 @@
 from sys import argv
 import random
-script, vdict, ndict, checkfile = argv
-v = open(vdict)
-vlines = v.readlines()
-n = open(ndict)
-nlines = n.readlines()
-v1 = random.randrange(0,7785)
-n1 = random.randrange(0,25271)
-v2 = vlines[v1].rstrip('\n')
-n2 = nlines[n1].rstrip('\n')
-c = open(checkfile, 'a+')
-c.write("%s The %s" % (v2.title(), n2.title()))
-c.write('\n')
-print "%s The %s" % (v2.title() , n2.title())
-clines = n.readlines()
+
+script, vdict, ndict, tweetfile = argv
+verbs = open(vdict)
+verblines = verbs.readlines()
+nouns = open(ndict)
+nounlines = nouns.readlines()
+tweets = open(tweetfile, 'a+')
+tweetlines = tweets.readlines()
+
+def maketweet(verblines, nounlines):
+	verbNum = random.randrange(0,7785)
+	nounNum = random.randrange(0,25271)
+	verbChoice = verblines[verbNum].rstrip('\n')
+	nounChoice = nounlines[nounNum].rstrip('\n')
+	tweet = "%s The %s" % (verbChoice.title(), nounChoice.title())
+	return tweet
+
+def checktweet(tweetlines, tweet, i):
+	for line in tweetlines:
+		tweetcheck = tweetlines[i].rstrip('\n')
+		if tweet == tweetcheck:
+			return False
+		i = i + 1
+	return True		
+
+tweeted = False
+while tweeted == False:
+	i = 0
+	tweet = maketweet(verblines, nounlines)
+	tweeted = checktweet(tweetlines, tweet, i)
+print tweet
+tweets.write(tweet)
+tweets.write('\n')
