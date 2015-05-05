e2164bc592508f13871c407995d29160a2984c41
NathanYo <nayocum@nevada.unr.edu>
2015-05-04 22:39:37 -0700

    Added full functionality to nate_bot.py

--- nate_bot.py
+++ nate_bot.py
@@ -1,15 +1,16 @@
 import tweepy, time, sys
 
 def read_file(argfile):
+    '''This function reads all of the lines of a given file'''
     filename = open(argfile, 'r')
     f = filename.readlines()
     filename.close()
     return f
 
-argfile = str(sys.argv[1])
-credential_arg = str(sys.argv[2])
+argfile = str(sys.argv[1]) # This is a list of tweets
+credential_arg = str(sys.argv[2]) # This is the text file with access tokens
 
-tweets_replied_to = "TWITTER TWEET IDS.txt"
+tweets_replied_to = "TWITTER TWEET IDS.txt" #TWITTER TWEET IDS.txt has a list of all tweets and DMs replied to
 
 cred = read_file(credential_arg)
 
@@ -24,9 +25,10 @@ api = tweepy.API(auth)
 
 
 def delete_line(argfile, del_line):
+    '''Rewrites the file without the parsed line'''
     filename = open(argfile, 'r+')
     f = filename.readlines()
-    filename.seek(0)
+    filename.seek(0) #Needs to select the very beginning to not write backwards
     for line in f:
         if line != del_line:
             filename.write(line)
@@ -35,26 +37,16 @@ def delete_line(argfile, del_line):
 
 
 def post_status():
+    '''Allows a status to be posted whenever it is called'''
     f = read_file(argfile)
     try:
         api.update_status(status=f[0])
         delete_line(argfile, f[0])
     except tweepy.error.TweepError:
-        print "ERROR: Duplicate Tweet"
-
-    #for line in f:
-    #    try:
-    #        api.update_status(status=line)
-    #        delete_line(argfile, line)
-    #    except tweepy.error.TweepError:
-    #        print "ERROR: Duplicate Tweet"
-    #    time.time.sleep(900) # Sleep for 10 minutes
-
-def get_timeline():
-    for status in tweepy.Cursor(api.user_timeline).items():
-        print status
+        print "ERROR: SENDING"
 
 def get_search_mentions():
+    '''Gets the last 100 mentions and checks if they have ever been replied to'''
     replied = read_file(tweets_replied_to)
     users = []
     mentions = api.mentions_timeline(count=100)
@@ -67,8 +59,23 @@ def get_search_mentions():
             name.close()
     return users
 
+def get_direct_messages():
+    '''Gets the last 100 direct messages and checks if they've been replied to'''
+    replied = read_file(tweets_replied_to)
+    users = []
+    direct_messages = api.direct_messages(count=100)
+    for dm in direct_messages:
+        message_id = str(dm.id) + '\n'
+        if message_id not in replied:
+            users.append(dm.sender.screen_name)
+            name = open(tweets_replied_to, 'a+')
+            name.write(message_id)
+            name.close()
+            print message_id
+    return users
 
 def post_replies():
+    '''Posts all of the needed replies'''
     f = read_file(argfile)
     users = get_search_mentions()
     for i in users:
@@ -77,34 +84,59 @@ def post_replies():
         try:
             api.update_status(status = tweet)
         except tweepy.error.TweepError:
-            print "ERROR: Duplicate Tweet"
+            print "ERROR SENDING"
 
-def post():
+def post_direct_message():
+    '''Posts all of the needed direct messages'''
+    f = read_file(argfile)
+    users = get_direct_messages()
+    for i in users:
+        message = f[0]
+        delete_line(argfile, message)
+        try:
+            api.send_direct_message(screen_name = i, text = message)
+        except tweepy.error.TweepError:
+            print "ERROR SENDING"
+
+def post(): #There's probably a cleaner way to do this...
+    '''Contains the posting sequence.'''
     post_status()
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
     post_replies()
-    time.sleep(900)
+    post_direct_message()
+    time.sleep(90)
 
 def main():
     post()
